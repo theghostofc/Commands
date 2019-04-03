acb208889b40c0c96cb3c2126ef05ff2d16338aa
Vivek Jain <theghostofc@gmail.com>
2019-04-03 12:35:19 +0530

    Uninstall Node JS on MacOS

---
+++ NodeJS/uninstall.sh
@@ -0,0 +1,6 @@
+sudo rm -rf $HOME/{local,lib,include,node*,npm,.npm*}
+sudo rm -rf /usr/local/lib/node*
+sudo rm -rf /usr/local/include/node*
+sudo rm -rf /usr/local/bin/{node,npm}
+sudo rm -rf /usr/local/share/man/man1/node.1
+sudo rm -rf /usr/local/lib/dtrace/node.d
